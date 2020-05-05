10a0a276201d024579efd5d6996a0832484c010a
Aniket Ghosh <ghoshaniketkumar7@gmail.com>
2020-05-05 11:37:30 +0530

    upgrade the methods

--- akPackageTest.java
+++ akPackageTest.java
@@ -5,5 +5,7 @@ public class akPackageTest {
         Ran ran=new Ran();
         System.out.println("Random number in between 50 to 100 is:"+ran.nextInt(50, 100));
         System.out.println("Random Email is:"+ran.getRandomEmail());
+        System.out.println("Random String is:"+ran.getString());
+        System.out.println("Random String is:"+ran.getString(30,"fbnbnhgyffjlsjflsafasdasdaskshfiusyfjshfksjdhfskhf"));
     }
 }
