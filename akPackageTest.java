dfe849c01bf6a21c9b4db6d66320b9f0a803e897
Aniket Ghosh <ghoshaniketkumar7@gmail.com>
2020-05-20 13:45:12 +0530

    array class is having a problem in the new method

--- akPackageTest.java
+++ akPackageTest.java
@@ -1,11 +1,12 @@
-import AkPackage.Ran;
-// all the akpackage testing will be done here
+import AkPackage.Array;
+
+/**
+ * akPackageTest
+ */
 public class akPackageTest {
+
     public static void main(String[] args) {
-        Ran ran=new Ran();
-        System.out.println("Random number in between 50 to 100 is:"+ran.nextInt(50, 100));
-        System.out.println("Random Email is:"+ran.getRandomEmail());
-        System.out.println("Random String is:"+ran.getString());
-        System.out.println("Random String is:"+ran.getString(30,"fbnbnhgyffjlsjflsafasdasdaskshfiusyfjshfksjdhfskhf"));
+        int T = Array.takeUserInputAsIntegerArray()[0];
+        System.out.println(T);
     }
 }
